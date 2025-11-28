a56136919b0571010a6cec25ce746f9b2a5aa7d9
Mikael <RIH4MN2@gmail.com>
2025-11-28 22:56:43 +0200

    Camera movement and head bob

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,9 +1,15 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.05
+const SENSITIVITY = 0.005
+
+#Headbob variablet
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
 var gravity = 9.8
 
@@ -28,15 +34,33 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.x * speed, delta * 7.0)
 	else:
-		velocity.x = 0.0
-		velocity.z = 0.0
-
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.5)
+		velocity.z = lerp(velocity.z, direction.x * speed, delta * 2.5)
+		
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
