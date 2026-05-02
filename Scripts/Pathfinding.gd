cf69a819ab6665d136c0034cec7996c77e2d002b
Mikael <RIH4MN2@gmail.com>
2026-05-02 16:18:38 +0300

    Pathfind testing

---
+++ Scripts/Pathfinding.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+
+var speed = 3
+var acceleration = 10
+
+@onready var navigationAgent := $NavigationAgent3D
+@onready var target := $"../Marker3D"
+@onready var targetNext := $"../Marker3D2"
+
+
+func _physics_process(delta):
+	var direction = Vector3()
+	
+	navigationAgent.target_position = target.global_position
+	
+	direction = navigationAgent.get_next_path_position() - global_position
+	direction = direction.normalized()
+	
+	velocity = velocity.lerp(direction * speed, acceleration * delta)
+	
+	if(navigationAgent.is_target_reached() == true):
+		nextTarget(delta)
+	else:
+		navigationAgent.target_position = target.global_position
+		
+	move_and_slide()
+	
+func nextTarget(_delta):
+	
+	navigationAgent.target_position = targetNext.global_position
+	
